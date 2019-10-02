b8980f46261e5e49ad81517737ab60c2b2db9783
Nick Bauer <nbauer@radford.edu>
2019-10-01 23:44:20 -0400

    attempting to scrape next page

--- vaguntrader_spider/spiders/posts.py
+++ vaguntrader_spider/spiders/posts.py
@@ -10,18 +10,21 @@ class PostsSpider(scrapy.Spider):
     #known URI's:
     #vaguntrader.com.../forums/3/<insert page number here>/Handguns_-_VIRGINIA_ONLY
     def parse(self, response):
-    	# topic-label is for location
-    	#posts = response.xpath('.//*[@class="topic-label"]')
+    	# topicsubject is interchanged with alt-topicsubject
     	posts = response.xpath('.//*[@class="topicsubject"]')
     	times = response.xpath('.//*[@class="topicsubject"]')
         # WORKS - locations = response.xpath('.//*[@class="topic-label"]/a/text()').extract()[i]
-        
-        i = 8
+        page = 2
+        i = 0
         print '\n############### STARTING SPIDER ###############\n'
         for post in posts:
        		location = response.xpath('.//span/a/text()').extract()[i]
        		#prints date in increments of 1
+       		#Date is not aligned correctly bc of their "...ago" addition
+       		# class date and time only exist for last posts past class new-alt-topictime
+       		# for second /3/2 the times are out of order
        		postDate = response.xpath('.//*[@class="date"]/text()').extract()[i]
+
        		postTime = response.xpath('.//*[@class="time"]/text()').extract()[i]
        		i += 1
        		postTitle = response.xpath('.//span/a/text()').extract()[i]
@@ -33,9 +36,6 @@ class PostsSpider(scrapy.Spider):
        		print '\n----------------------------------------------\n'
 
        	print '\n############### CLOSING SPIDER ###############\n'
-        #for post in posts:
-        #	location = response.xpath('.//a/text()').extract()[i]
-        #	print '\n'
-        #	print location
-        #	print '\n'
-        #	i += 1
+
+       	next_page_url = response.urljoin('https://vaguntrader.com/forums/ubbthreads.php/forums/3/3/Handguns_-_VIRGINIA_ONLY/')
+       	yield scrapy.Request(next_page_url)
